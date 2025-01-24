79937aeb080b79534665cb26d67ca5fe3ec49384
RouzbehAB <roozbeh7abadi@gmail.com>
2025-01-24 17:39:13 +0330

    notification entity has been completed !

---
+++ src/main/notification/models/Notification.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from src.main.database.da import Base
+from src.main.users.models.associations import user_role_table
+
+
+class Notification(Base):
+    __tablename__ = "notification"
+    title = Column("title", String(50), nullable=True)
+    content = Column("content", String(50), nullable=True)
+    notification_type = Column("type", String(50), nullable=True)
+    target_user = Column("target user")
+    creation_time = Column(DateTime, default=datetime.now)
+
+    def __init__(self, title, content, notification_type, target_user, creation_time):
+        self.title = title
+        self.content = content
+        self.notification_type = notification_type
+        self.target_user = target_user
+        self.creation_time = creation_time
