72d03a5bae1988ce9a165cc7de10672deae178c9
MahsaArya <mahsa3.arya@gmail.com>
2025-01-24 16:46:45 +0330

    updated

--- src/main/ticketing/models/Ticket_Group.py
+++ src/main/ticketing/models/Ticket_Group.py
@@ -5,13 +5,12 @@ from src.main.database.da import Base
 
 class TicketGroup(Base):
     __tablename__ = ('ticketgroup')
-    _id = Column(Integer,primary_key=True, autocrement=True)
+    _id = Column(Integer, primary_key=True, autocrement=True)
     _name = Column(String(100), nullable=False)
     _parent_id = Column(Integer, ForeignKey('ticket_group.id'), nullable=True)
     _child_id = Column(Integer, ForeignKey('ticket_group.id'), nullable=True)
 
     parent = relationship('TicketGroup', remote_side=id)
-
 
     def __init__(self, name, parent_id, child_id):
         self.name = name
