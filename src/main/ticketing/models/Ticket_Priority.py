c2a3e08255f33f2817be993ace6f2f39d01b071e
panizabt <abootalebipaniz@gmail.com>
2025-01-24 10:51:37 +0330

    Update Ticket_Priority.py

--- src/main/ticketing/models/Ticket_Priority.py
+++ src/main/ticketing/models/Ticket_Priority.py
@@ -1,1 +1,15 @@
+from sqlalchemy import Column, string, Integer, DateTime, Foreignkey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from src.main.database.da import Base
 
+class TicketPriority(Base):
+    __tablename__ = "ticketpriority"
+    id = Column(Integer, primary_key=True)
+    priorityName = Column(string(50), nullable=False)
+
+    ticket = relationship("Ticket", back_populates="priority")
+    
+    
+    def __init__(self, priorityname):
+        self.priorityName = priorityname
