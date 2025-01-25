ca908e6b216f10909d66650b9c9cee71ad03d66d
panizabt <abootalebipaniz@gmail.com>
2025-01-25 22:20:01 +0330

    Update app.py

--- src/main/ticketing/app.py
+++ src/main/ticketing/app.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from src.main.message.controller import route as message_route
 
+app = FastAPI(title="Message Management API", version="1.0")
+
+app.include_router(message_route)
+
+@app.get("/")
+def root():
+    return {"message": "welcome to Message Management API"}
