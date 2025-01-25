37d49a5dbafea9e45c5d276f3a627b2362536ce9
Taha Ghanbari birgani <ghanbariitaha@gmail.com>
2025-01-25 13:18:12 +0330

    Update __init__.py

--- src/main/organization/models/__init__.py
+++ src/main/organization/models/__init__.py
@@ -2,4 +2,4 @@ from src.main.organization.models.organization import Organization
 from src.main.organization.models.department import Department
 
 
-__all__ = ["Organization","Department"]
+__all__ = ["Organization","Department","Section"]
