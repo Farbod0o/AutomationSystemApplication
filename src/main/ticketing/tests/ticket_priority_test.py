f9ee155df8ea680c1d8608ee0cfbf2c81717917e
panizabt <abootalebipaniz@gmail.com>
2025-01-25 23:05:48 +0330

    Update ticket_priority_test.py

--- src/main/ticketing/tests/ticket_priority_test.py
+++ src/main/ticketing/tests/ticket_priority_test.py
@@ -1,1 +1,11 @@
+from fastapi.testclient import TestClient
+from src.main.ticket_priority.app import app
 
+
+client = TestClient(app)
+
+def test_creat_ticket_priority():
+    response = client.post("/ticket_priority/", json={"priority": "high"})
+    assert response.status_code == 200
+
+    print("Priority created successfully")
