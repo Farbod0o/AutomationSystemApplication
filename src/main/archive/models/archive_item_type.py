4891846d24f92819e0e5ae36fdda8b287d3175b6
alifathi1x <ali.abolfathee@yahoo.com>
2025-01-27 21:42:30 +0330

    adedd by ali fathi

--- src/main/archive/models/archive_item_type.py
+++ src/main/archive/models/archive_item_type.py
@@ -7,3 +7,6 @@ class ArchiveItemType(Base):
     __tablename__ = "ArchiveItemType"
     id = Column("id", Integer, primary_key=True)
     type = Column("type", String(50), nullable=False)
+
+    def __init__(self,type):
+        self.type = type
