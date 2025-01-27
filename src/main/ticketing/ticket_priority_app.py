b0a5c3261cd4c2a909abcc73ddd527ae6ece82bf
panizabt <abootalebipaniz@gmail.com>
2025-01-27 23:14:47 +0330

    Update ticket_priority_app.py

--- src/main/ticketing/ticket_priority_app.py
+++ src/main/ticketing/ticket_priority_app.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from src.main.ticket_priority.controller import route as ticket_priority_route
 
-app = FastAPI(title="Priority Management API", version="1.0"
+app = FastAPI(title="Priority Management API", version="1.0")
 
 app.include_router(ticket_priority_route)
 
-@app.get("/"
+@app.get("/")
 def root():
      return {"message": "Welcome to Priority Management API"}
 
