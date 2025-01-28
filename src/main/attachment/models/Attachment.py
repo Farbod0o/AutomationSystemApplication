bbb23c1dc4ed3611b5f538273f2443894f3206db
RouzbehAB <roozbeh7abadi@gmail.com>
2025-01-28 16:34:09 +0330

    notification entity has been updated ! attachment entity has been updated ! statusupdate entity has been updated ! message entity has been updated ! chat entity has been updated !

--- src/main/attachment/models/Attachment.py
+++ src/main/attachment/models/Attachment.py
@@ -6,18 +6,21 @@ from src.main.users.models.associations import user_role_table
 
 
 class Attachment(Base):
-    __tablename__ = "attachment"
-    sent_date= Column("sent_date", datetime, nullable=False)
-    sender = Column("sender", String(100), nullable=False)
-    file = Column("file", String(100),  nullable=True)
-    file_size= Column("file_size", float(10), nullable=False)
-    file_format = Column("file_format", String(100), nullable=False)
+    __tablename__ = "attachment_tbl"
 
-    chat = relationship("Chat", uselist=False, back_populates="chat")
+    _attachment_id = Column(Integer, primary_key=True, autoincrement=True)
+    _sent_date = Column("sent_date", datetime, nullable=False)
+    _sender = Column("sender", String(100), nullable=False)
+    _file = Column("file", String(100),  nullable=True)
+    _file_size = Column("file_size", float(4), nullable=False)
+    _file_format = Column("file_format", String(100), nullable=False)
+
+    chat_id = Column(Integer, ForeignKey('chat_id'))
+    chat = relationship("Chat", uselist=False, back_populates="attachment_tbl")
 
     def __init__(self, sent_date, sender, file, file_size, file_format):
-        self.sent_date = sent_date
-        self.sender = sender
-        self.file = file
-        self.file_size = file_size
-        self.file_format = file_format
+        self._sent_date = sent_date
+        self._sender = sender
+        self._file = file
+        self._file_size = file_size
+        self._file_format = file_format
---
+++ src/main/message/models/Message.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from src.main.database.da import Base
+from src.main.users.models.associations import user_role_table
+
+class Message(Base):
+    __tablename__ = "message_tbl"
+
+    _message_id = Column("id", Integer, primary_key=True, autoincrement=True)
+    _send_date = Column("SendDate", DateTime)
+    _sender = Column("Sender", String(30))
+    _text = Column("Text", String(50))
+
+    notification = relationship("Notification", uselist=False, back_populates="message_tbl")
+
+    chat_id = Column(Integer, ForeignKey("chat_id"))
+    chat = relationship("Chat", back_populates="message_tbl")
+def __init__(self, send_date, sender, text):
+    self._send_date = send_date
+    self._sender = sender
+    self._text = text
+
+def __repr__(self):
+    return f"<Message(id={self.id}, send_date='{self.send_date}', sender='{self.sender}', text='{self.text}')>', " \
