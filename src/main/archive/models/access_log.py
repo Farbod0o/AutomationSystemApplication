fe339c27189de46f32558a84dd42d938c21b4801
ariitooo <137182057+ariitooo@users.noreply.github.com>
2025-01-30 09:46:56 +0330

    Update access_log.py

--- src/main/archive/models/access_log.py
+++ src/main/archive/models/access_log.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 class AccessLog(Base):
     __tablename__ = "AccessLog"
     id = Column("id", Integer, primary_key=True)
+    access_log_id= Column("id", Integer, primary_key=True)
     accessDate = Column("accessDate", DateTime, nullable=False)
 
     def __init__(self, accessDate):
