f3e566914c218bb6ddf710a460ed8b76ac3326c6
panizabt <abootalebipaniz@gmail.com>
2025-01-30 18:23:25 +0330

    Update asset_app.py

--- src/main/asset management/asset_app.py
+++ src/main/asset management/asset_app.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from src.main.asset_management.controller.asset_controller import route as asset_route
+
+app = FastAPI(title="Asset Management API", version="1.0")
+app.include_router(asset_route)
+
+@app.get("/")
+def root():
+        return {"message": "Welcome to Asset Management API"}
+
+
 
