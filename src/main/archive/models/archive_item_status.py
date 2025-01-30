1e228358297396efd05a98052c7a53af2cafc92d
ariitooo <137182057+ariitooo@users.noreply.github.com>
2025-01-30 10:40:04 +0330

    Update archive_item_status.py

--- src/main/archive/models/archive_item_status.py
+++ src/main/archive/models/archive_item_status.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import relationship
 
 class ArchiveItemStatus(Base):
     id = Column("id", Integer, primary_key=True)
+    archive_item_status_id= Column("id", Integer, primary_key=True)
     statusName = Column("statusName", String(30), nullable=False)
 
     def __init__(self, statusName):
