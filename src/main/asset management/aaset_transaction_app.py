7f716d3d396e9272e535c26cb70f5e88a6fb98f4
panizabt <abootalebipaniz@gmail.com>
2025-01-30 18:25:12 +0330

    Update aaset_transaction_app.py

--- src/main/asset management/aaset_transaction_app.py
+++ src/main/asset management/aaset_transaction_app.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from src.main.asset_management.controller.asset_transection_controller import route as asset_transection_route
 
+app = FastAPI(title="Asset Transection App",version="1.0")
+app.include_router(asset_transection_route)
+
+@app.get("/")
+def root():
+    return {"message": "welcome to Asset Transection App"}
