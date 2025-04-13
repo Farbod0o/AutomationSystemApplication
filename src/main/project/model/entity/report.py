e0b48d7bb3581e9730c25d5e86bb9386f7bc3279
RouzbehAB <roozbeh7abadi@gmail.com>
2025-04-13 15:37:18 +0330

    project entities has been created !

---
+++ src/main/project/model/entity/report.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey ,Float
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from src.main.database.da import Base
+from enum import Enum as PyEnum, Enum
+
+class Status(PyEnum):
+    PLANNED = "Planned"
+    INPROGRESS = "In Progress"
+    COMPLETED = "Completed"
+    CANCELED = "Canceled"
+
+class Type(PyEnum):
+    WEEKLY = "Weekly"
+    MONTHLY = "Monthly"
+    DAILY = "Daily"
+    FINALS = "Finals"
+    INCIDENT = "Incident"
+
+
+class Report(Base):
+    __tablename__ = "tasks"
+
+    report_id = Column("report_ID", Integer, primary_key=True, autoincrement=True)
+    author = Column("author", Integer, ForeignKey("users.id"), nullable=False)
+    type = Column("type", Enum(Type), nullable=False, default=Type.DAILY)
+    name = Column("task_name", String(30),  nullable=False)
+    description = Column("description", String(100), nullable=False)
+    status = Column("status",Enum(Status) ,nullable=False, default=Status.PLANNED)
+    date = Column("date", DateTime, nullable=False)
+
+    ### relaitions to do
+
+    def __init__(self, report_id, author, type, name, description, status, date):
+        self.report_id = report_id
+        self.author = author
+        self.type = type
+        self.name = name
+        self.description = description
+        self.status = status
+        self.date = date
+
--- src/main/project/model/entity/sub_project.py
+++ src/main/project/model/entity/sub_project.py
@@ -10,34 +10,26 @@ class Status(PyEnum):
     COMPLETED = "Completed"
     CANCELED = "Canceled"
 
+class SubProject(Base):
+    __tablename__ = "sub_projects"
 
-class Project(Base):
-    __tablename__ = "projects"
-
-    project_id = Column("project_ID", Integer, primary_key=True, autoincrement=True)
-    project_name = Column("name",  String(30), nullable=False)
+    sub_project_id = Column("sub_project_ID", Integer, primary_key=True, autoincrement=True)
+    name = Column("sub_project_name", String(30),  nullable=False)
     description = Column("description", String(100), nullable=False)
     status = Column("status",Enum(Status),nullable=False, default=Status.PLANNED)
-    start_date = Column("start_date", DateTime, nullable=False)
-    end_date = Column("end_date", DateTime, nullable=False)
-    registration_date = Column("registration_date", DateTime, nullable=False)
-    # approvers = Column("approvers", Integer, ForeignKey("users.id"), nullable=False)
-    # priority = Column("priority", String(30), nullable=False)
+    parent_project = Column("parent_project", Integer, ForeignKey("projects.id"), nullable=False)
 
     # message = relationship("", back_populates="")
     # notification = relationship("", back_populates="")
     # attachment = relationship("", back_populates="")
     # status_update = relationship("", back_populates="")
-    #
+
     # user_id = Column(Integer, ForeignKey("users.id"))
     # user = relationship("User", back_populates="Chats")
 
-    def __init__(self, project_id, project_name, description, status, start_date, end_date, registration_date):
-        self.project_id = project_id
-        self.project_name = project_name
+    def __init__(self, sub_project_id, name, description, status, parent_project):
+        self.sub_project_id = sub_project_id
+        self.name = name
         self.description = description
         self.status = status
-        self.start_date = start_date
-        self.end_date = end_date
-        self.registration_date = registration_date
-
+        self.parent_project = parent_project
